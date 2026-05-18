7bb595d9fd89557527c5c542c733e3dab38c94f3
emanoelrmorais-lab <emanoelrmorais@gmail.com>
2026-05-18 14:12:28 -0300

    test fraction

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace FractionApp
+{
+    public class Fraction
+    {        private int _top;
+             private int _bottom;
+
+        public Fraction(int top)
+        {
+            _top = 1;
+            _bottom = 1;
+        }
+
+        public Fraction(int top,int bottom)
+        {
+            _top = top;
+            if (bottom == 0)
+            {
+                throw new ArgumentException ("Bottom cannot be zero");
+            }   
+           
+            _bottom = bottom;
+        }
+
+        public int GetTop()
+        {
+            return _top;
+        }
+
+        public void SetTop(int top)
+        {
+            _top = top;
+        }
+
+        // Getters e Setters para o valor inferior (Bottom)
+        public int GetBottom()
+        {
+            return _bottom;
+        }
+
+        public void SetBottom(int bottom)
+        {
+            if (bottom == 0)
+            {
+                throw new ArgumentException("O número inferior (bottom) não pode ser zero.");
+            }
+            _bottom = bottom;
+        }
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
-class Program
+
+
+namespace FractionApp
 {
-    static void Main(string[] args)
+    class  Program
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction();
+            Fraction f2 = new Fraction(6);
+            Fraction f3 = new Fraction(6 , 7);
+
+            Fraction testGetSet = new Fraction();
+            testGetSet.SetTop(3);
+            testGetSet.SetBotton(4);
+
+            Console.WriteLine(" --- Test Getters and Setters ---");
+            Console.WriteLine($"Top modified: {testGetSet.GetTop()}");
+            Console.WriteLine($"Bottom modified: {testGetSet.GetBotton()}");
+            Console.WriteLine();
+
+
+            Console.WriteLine("---Sample Output(Sample Output) ---");
+
+            Fraction case1 = new Fraction();
+            Console.WriteLine(case1.GetFractionString());
+            Console.WriteLine(case1.GetDecimalValue());
+
+            Fraction case2 = new Fraction(5);
+            Console.WriteLine(case2.GetFractionString());
+            Console.WriteLine(case2.GetDecimalValue());
+
+            Fraction case3 = new Fraction(3, 4);
+            Console.WriteLine(case3.GetFractionString());
+            Console.WriteLine(case3.GetDecimalValue());
+
+            Fraction case4 = new Fraction(1, 3);
+            Console.WriteLine(case4.GetFractionString());
+            Console.WriteLine(case4.GetDecimalValue());
+
+
+    
+        }
     }
 }
+
