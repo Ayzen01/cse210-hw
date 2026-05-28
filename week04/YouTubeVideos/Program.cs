e0b70aaca1d71d6d91e5cc4ea2712ef574ef2adf
emanoelrmorais-lab <emanoelrmorais@gmail.com>
2026-05-28 09:51:48 -0300

    printscreen youtube task

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+
+        Video video = new Video("Learning C#", "John Davis", 300);
+
+        Comment c1 = new Comment("Mary", "It's a nice video! I learned a lot.");
+        Comment c2 = new Comment("David S" , "Great explanation! Thanks for sharing.");
+        Comment c3 = new Comment("Sarah", "Very helpful tutorial. Keep up the good work!");
+        
+        video.AddComment(c1);
+        video.AddComment(c2);
+        video.AddComment(c3);
+        video.DisplayVideoINFo();
     }
 }
