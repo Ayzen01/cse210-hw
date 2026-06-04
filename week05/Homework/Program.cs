81cbb1b94f563dd98be95b126dfbed345cf33765
emanoelrmorais-lab <emanoelrmorais@gmail.com>
2026-06-04 09:46:41 -0300

    up dates math, writing, base assignments

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        
+        MathAssignment math = new MathAssignment("John", "Fraction", "section 7.3", "Problemas 8-12");
+        WritingAssignment writing = new WritingAssignment("Jane", "History", "The Hunter");
+        
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetMathInfo());
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInfo());
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
+
 }
