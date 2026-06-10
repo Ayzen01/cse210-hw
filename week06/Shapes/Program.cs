1353031361bd585c4c05bf2652d4895a9620bacd
emanoelrmorais-lab <emanoelrmorais@gmail.com>
2026-06-10 10:36:20 -0300

    shapes assingment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red",5);
+        Rectangle rectangle = new Rectangle("Blue",4, 6);
+        Circle circle = new Circle("Green",3);
+
+        Console.WriteLine($"Square  color: {square.GetColor()}, area: {square.GetArea()}");
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}, area: {rectangle.GetArea()}");
+        Console.WriteLine($"Circle  color: {circle.GetColor()}, area: {circle.GetArea()}");
+
+
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        Console.WriteLine("\nAll shapes in the list:");
+
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+
+        }
+        
+
     }
 }
